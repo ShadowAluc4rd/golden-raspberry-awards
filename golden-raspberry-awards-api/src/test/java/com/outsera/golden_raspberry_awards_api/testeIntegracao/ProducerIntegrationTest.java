e3f97e60eb5178ced32a6db1526ee14f070dc5ef
de Barros Brito <endb@gft.com>
2024-09-17 16:58:49 -0300

    Ajuste Teste Integrado para garantir resultado da API

--- golden-raspberry-awards-api/src/test/java/com/outsera/golden_raspberry_awards_api/testeIntegracao/ProducerIntegrationTest.java
+++ golden-raspberry-awards-api/src/test/java/com/outsera/golden_raspberry_awards_api/testeIntegracao/ProducerIntegrationTest.java
@@ -1,13 +1,21 @@
 package com.outsera.golden_raspberry_awards_api.testeIntegracao;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.outsera.golden_raspberry_awards_api.model.ProducerResponse;
+import org.json.JSONException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ProducerIntegrationTest {
@@ -15,14 +23,25 @@ public class ProducerIntegrationTest {
     @Autowired
     private TestRestTemplate restTemplate;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @Test
-    public void testGetProducersIntervals() {
+    public void testGetProducersIntervals() throws IOException, JSONException {
+        String expectedJson = new String(Files.readAllBytes(Paths.get("src/test/resources/expected_response.json")));
+
         ResponseEntity<ProducerResponse> response = restTemplate.getForEntity("/api/producers/intervals", ProducerResponse.class);
 
         Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
 
         ProducerResponse producerResponse = response.getBody();
-        Assertions.assertNotNull(producerResponse);
+        Assertions.assertNotNull(producerResponse, "A resposta da API não deveria ser nula.");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String responseJson = objectMapper.writeValueAsString(producerResponse);
+
+        // Comparar os JSONs
+        JSONAssert.assertEquals(expectedJson, responseJson, JSONCompareMode.STRICT);
     }
 
     @Test
